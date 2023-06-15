593e68298ad26c1a3c467518322827ec82745e19
dsidorcz <01169234@pw.edu.pl>
2023-06-15 14:45:50 +0200

    Added slider for E in html

--- CRT.js
+++ CRT.js
@@ -1,20 +1,22 @@
-let E = -1e-9; // V/m
+var E = 0; // V/m
 let dt = 0.01;
 let es = [];
 let width = 1200;
 let height = 800;
-let canvas;
-let updateCanvas;
+var canvas;
+var updateCanvas;
+var change_E;
+var actual_E;
 
 class electron {
     constructor(posX, posY, R) {
         this.pos = {x: posX, y: posY};
-        this.v = 0; // prędkość m/s
+        this.v = 0; // prędkość [m/s]
         this.alpha = 0; // kąt odchylenia od osi x
         this.r = R || 10;
-        this.q = -1.602e-19;  // C
-        this.m = 9.1e-31;  // kg
-        this.execution = 0;
+        this.q = -1.602e-19;  // ładunek [C]
+        this.m = 9.1e-31;  // [kg]
+        this.execution = 0; // jak wyjdzie poza ekran to zrobić rach ciach ciach
     }
 
     draw() {
@@ -35,7 +37,12 @@ class electron {
         }
         let a = this.q * E / this.m;
         vX += a * dt;
-        this.alpha = atan(vY / vX);
+        if (vX != 0) {
+            this.alpha = atan(vY / vX);
+        }
+        else {
+            this.alpha = 0;
+        }
         this.v = (vX**2 + vY**2)**(1/2);
     }
 }
@@ -45,6 +52,15 @@ function setup() {
     canvas = createCanvas(width, height);
     updateCanvas = document.getElementById('update_Canvas');
     updateCanvas.addEventListener('click', update_Canvas);
+    change_E = document.getElementById('Eslider');
+    change_E.addEventListener("change", update_E);
+    actual_E = document.getElementById("E_value");
+    actual_E.textContent = 0;
+}
+
+function update_E() {
+    E = -change_E.value;
+    actual_E.textContent = E;
 }
 
 function update_Canvas() {
